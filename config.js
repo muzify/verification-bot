ac0e952394a2141a2398daeda0fa48b2b147b31d
fortnitelookup <150611594+muzify@users.noreply.github.com>
2024-04-10 19:34:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	bot: {
-		token: 'MTIyNzY3MjA5MjMxNzcxNjU5Mg.GyDKuE.nzNMen3jAv5euTyHpDbj29Fba5duMJ5G1zsX3o',
+		token: 'MTIyNzY3MjA5MjMxNzcxNjU5Mg.GpTnsb.6NjBP8ZL_Ye_Hd5aqCD6j2wthUZ_glKyYomJYM',
 		prefix: '!',
 		clientSecret: 'WRCHB8hlWGZ1zKRnbPSUccpqXIHrY-hM',
 		clientID: '1227672092317716592'
